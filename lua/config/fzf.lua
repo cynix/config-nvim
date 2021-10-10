f8d025b70d45a9f1cbecf87c207f51c7dae91b28
Brian Chu <brian.chu@optiver.com.au>
2021-10-10 15:39:10 +1100

    Automatically select the only result for LSP actions

--- lua/config/fzf.lua
+++ lua/config/fzf.lua
@@ -34,10 +34,16 @@ fzf.setup({
 
 vim.api.nvim_set_keymap('n', '<C-p>', [[<cmd>lua require('config.fzf').files()<CR>]], {noremap=true, silent=true})
 
+local function select_one(fn)
+  return function()
+    return fn({fzf_opts={['--select-1']=''}})
+  end
+end
+
 local handlers = vim.lsp.handlers
-handlers['textDocument/codeAction'] = fzf.lsp_code_actions
-handlers['textDocument/definition'] = fzf.lsp_definitions
-handlers['textDocument/implementation'] = fzf.lsp_implementations
-handlers['textDocument/references'] = fzf.lsp_references
+handlers['textDocument/codeAction'] = select_one(fzf.lsp_code_actions)
+handlers['textDocument/definition'] = select_one(fzf.lsp_definitions)
+handlers['textDocument/implementation'] = select_one(fzf.lsp_implementations)
+handlers['textDocument/references'] = select_one(fzf.lsp_references)
 
 return M
