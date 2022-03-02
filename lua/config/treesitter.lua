148393c2a6685b3b6b4a05bb1a83cf8c1e36d8fd
cynix <cynix@cynix.org>
2022-03-02 13:07:20 +1100

    Remove tree-sitter-cpp override

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -1,7 +1,3 @@
-local cpp = require('nvim-treesitter.parsers').get_parser_configs().cpp.install_info
-cpp.url = 'https://github.com/tree-sitter/tree-sitter-cpp.git'
-cpp.revision = 'a7652fce5943c9d5d9c49dd8e3256a699aa33bf5'
-
 local config = {
   ensure_installed = {
     'bash',
