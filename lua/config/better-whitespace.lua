a7499875f16434c84e169b50c47eaaa697a9ab59
cynix <cynix@cynix.org>
2022-04-06 10:26:52 +1000

    Rename LspTrouble to Trouble

--- lua/config/better-whitespace.lua
+++ lua/config/better-whitespace.lua
@@ -1,5 +1,5 @@
 local g = vim.g
-g.better_whitespace_filetypes_blacklist = {'diff', 'gitcommit', 'help', 'LspTrouble', 'markdown', 'packer', 'qf'}
+g.better_whitespace_filetypes_blacklist = {'diff', 'gitcommit', 'help', 'Trouble', 'markdown', 'packer', 'qf'}
 g.show_spaces_that_precede_tabs = 1
 
 require('utils.highlight').hl({'ExtraWhitespace', fg='#870000'})
--- lua/config/trouble.lua
+++ lua/config/trouble.lua
@@ -1,3 +1,3 @@
 require('trouble').setup()
 
-require('utils.map').map({'n', 'i'}, '<F1>', '<cmd>LspTroubleToggle<CR>', {noremap=true, silent=true})
+require('utils.map').map({'n', 'i'}, '<F1>', '<cmd>TroubleToggle<CR>', {noremap=true, silent=true})
