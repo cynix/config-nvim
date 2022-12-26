e327ef45ae01a8344049392d41f420e76212e201
cynix <cynix@cynix.org>
2022-12-26 13:31:20 +1100

    Update fzf-lua config

--- lua/config/fzf.lua
+++ lua/config/fzf.lua
@@ -34,6 +34,8 @@ fzf.setup({
 
 vim.api.nvim_set_keymap('n', '<C-p>', [[<cmd>lua require('config.fzf').files()<CR>]], {noremap=true, silent=true})
 
+fzf.register_ui_select()
+
 local function select_one(fn)
   return function()
     return fn({fzf_opts={['--select-1']=''}})
@@ -41,9 +43,15 @@ local function select_one(fn)
 end
 
 local handlers = vim.lsp.handlers
+handlers['callHierarchy/incomingCalls'] = select_one(fzf.lsp_incoming_calls)
+handlers['callHierarchy/outgoingCalls'] = select_one(fzf.lsp_outgoing_calls)
 handlers['textDocument/codeAction'] = fzf.lsp_code_actions
+handlers['textDocument/declaration'] = select_one(fzf.lsp_declarations)
 handlers['textDocument/definition'] = select_one(fzf.lsp_definitions)
+handlers['textDocument/documentSymbol'] = fzf.lsp_document_symbols
 handlers['textDocument/implementation'] = select_one(fzf.lsp_implementations)
 handlers['textDocument/references'] = select_one(fzf.lsp_references)
+handlers['textDocument/typeDefinition'] = fzf.lsp_typedefs
+handlers['workspace/symbol'] = fzf.lsp_workspace_symbols
 
 return M
