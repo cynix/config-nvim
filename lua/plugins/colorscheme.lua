bb48b5e34af6fd7d494725482721856c3d40f8fb
cynix <cynix@cynix.org>
2023-04-09 23:19:32 +1000

    Dim inactive windows

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,3 +1,5 @@
+local api = vim.api
+
 return {
   {
     'LazyVim/LazyVim',
@@ -8,7 +10,7 @@ return {
           require('utils.autocmd').augroup('CustomHighlights', function(autocmd)
             autocmd('ColorScheme', '*', function()
               local c = require('gruvbox-baby.colors').config()
-              local hl = vim.api.nvim_set_hl
+              local hl = api.nvim_set_hl
 
               hl(0, 'NonText', {fg=c.medium_gray})
 
@@ -16,7 +18,7 @@ return {
               hl(0, 'LspReferenceRead', {link='LspReferenceText'})
               hl(0, 'LspReferenceWrite', {link='LspReferenceText'})
 
-              hl(0, 'LspCodeLens', {fg=vim.api.nvim_get_hl_by_name('Comment', true).foreground})
+              hl(0, 'LspCodeLens', {fg=api.nvim_get_hl_by_name('Comment', true).foreground})
 
               hl(0, 'IlluminatedWordText', {link='LspReferenceText'})
               hl(0, 'IlluminatedWordRead', {link='LspReferenceText'})
@@ -28,6 +30,20 @@ return {
     },
     opts = {
       colorscheme = 'gruvbox-baby',
+    },
+  },
+  {
+    'levouh/tint.nvim',
+    event = 'VeryLazy',
+    opts = {
+      tint = -20,
+      saturation = 0.5,
+      highlight_ignore_patterns = {'WinSeparator', 'Status.*'},
+      window_ignore_function = function(winid)
+        local floating = api.nvim_win_get_config(winid).relative ~= ''
+        local buftype = api.nvim_buf_get_option(api.nvim_win_get_buf(winid), 'buftype')
+        return floating or buftype == 'terminal'
+      end,
     },
   },
 }
