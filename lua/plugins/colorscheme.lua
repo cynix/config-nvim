07807364ba88eb22812b1a979d63cbb579c15c53
cynix <cynix@cynix.org>
2023-04-09 10:58:14 +1000

    Improve LSP highlights

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,20 +5,21 @@ return {
       {
         'luisiacc/gruvbox-baby',
         config = function()
-          local hl = vim.api.nvim_set_hl
-          hl(0, 'LspReferenceText', {fg='#fbf1c7', bg='#005f87'})
-          hl(0, 'LspReferenceRead', {link='LspReferenceText'})
-          hl(0, 'LspReferenceWrite', {link='LspReferenceText'})
+          require('utils.autocmd').augroup('CustomHighlights', function(autocmd)
+            autocmd('ColorScheme', '*', function()
+              local hl = vim.api.nvim_set_hl
 
-          vim.api.nvim_create_autocmd('ColorScheme', {
-            group = vim.api.nvim_create_augroup('IlluminatedHighlights', {clear=true}),
-            pattern = '*',
-            callback = function()
+              hl(0, 'LspReferenceText', {fg='#fbf1c7', bg='#005f87'})
+              hl(0, 'LspReferenceRead', {link='LspReferenceText'})
+              hl(0, 'LspReferenceWrite', {link='LspReferenceText'})
+
+              hl(0, 'LspCodeLens', {fg=vim.api.nvim_get_hl_by_name('Comment', true).foreground})
+
               hl(0, 'IlluminatedWordText', {link='LspReferenceText'})
               hl(0, 'IlluminatedWordRead', {link='LspReferenceText'})
               hl(0, 'IlluminatedWordWrite', {link='LspReferenceText'})
-            end,
-          })
+            end)
+          end)
         end,
       },
     },
