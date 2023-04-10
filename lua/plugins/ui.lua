5f35683225228d52824adbdc96c0214989f0dbb7
cynix <cynix@cynix.org>
2023-04-10 14:18:35 +1000

    Add incline.nvim

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,4 +1,54 @@
+local api = vim.api
+
+local function get_diagnostic_label(props)
+  local icons = {
+    Error = '',
+    Warn = '',
+    Info = '',
+    Hint = '',
+  }
+
+  local label = {}
+
+  for severity, icon in pairs(icons) do
+    local n = #vim.diagnostic.get(props.buf, {severity = vim.diagnostic.severity[string.upper(severity)]})
+    if n > 0 then
+      table.insert(label, {icon .. ' ' .. n .. ' ', guifg=vim.api.nvim_get_hl_by_name('DiagnosticSign' .. severity, true).foreground})
+    end
+  end
+
+  return label
+end
+
 return {
+  {
+    'b0o/incline.nvim',
+    dependencies = 'luisiacc/gruvbox-baby',
+    event = 'VeryLazy',
+    opts = {
+      render = function(props)
+        local filename = vim.fn.fnamemodify(api.nvim_buf_get_name(props.buf), ':t')
+        local diagnostics = get_diagnostic_label(props)
+        local modified = api.nvim_buf_get_option(props.buf, 'modified') and 'bold,italic' or 'None'
+        local filetype_icon, color = require('nvim-web-devicons').get_icon_color(filename)
+        local colors = require('gruvbox-baby.colors').config()
+
+        local buffer = {
+          { filetype_icon, guifg=color },
+          { ' ' },
+          { filename, gui=modified, guifg=(props.focused and colors.foreground or colors.comment) },
+        }
+
+        if #diagnostics > 0 then
+          table.insert(diagnostics, { '| ', guifg='grey' })
+        end
+        for _, buffer_ in ipairs(buffer) do
+          table.insert(diagnostics, buffer_)
+        end
+        return diagnostics
+      end,
+    },
+  },
   {
     'echasnovski/mini.indentscope',
     opts = function(_, opts)
