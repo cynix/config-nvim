bf9c171ada903743be9bd42e4e9678c2f1f5da02
cynix <cynix@cynix.org>
2023-04-12 09:57:01 +1000

    Disable unused colorschemes

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,4 +1,6 @@
 return {
+  { 'catppuccin/nvim', enabled = false },
   { 'echasnovski/mini.surround', enabled = false },
+  { 'folke/tokyonight.nvim', enabled = false },
   { 'goolord/alpha-nvim', enabled = false },
 }
