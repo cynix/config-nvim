028cd8e317cdc80f6fbb01e40fd46ad77fc23338
cynix <cynix@cynix.org>
2023-04-12 09:54:34 +1000

    Add keymap descriptions

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,47 +1,65 @@
-local map = vim.keymap.set
+local register = require('which-key').register
 local api = vim.api
 
 -- add blank line above/below
-map('n', '-', "<Cmd>put=''<CR>", {noremap=true})
-map('n', '+', "<Cmd>put!=''<CR>", {noremap=true})
+register({
+  ['-'] = { "<Cmd>put=''<CR>", 'Empty line below' },
+  ['+'] = { "<Cmd>put!=''<CR>", 'Empty line above' },
+})
 
 -- visual indent/unindent
-map('x', '<Tab>', '>gv', {noremap=true})
-map('x', '<S-Tab>', '<gv', {noremap=true})
+register({
+  ['<Tab>'] = { '>gv', 'Indent' },
+  ['<S-Tab>'] = { '<gv', 'Undent' },
+}, {
+  mode = 'x',
+})
 
 -- use regex search by default
-map({'n', 'x'}, '/', '/\\v', {noremap=true})
-map({'n', 'x'}, '?', '?\\v', {noremap=true})
+register({
+  ['/'] = { '/\\v', 'Search forward' },
+  ['?'] = { '?\\v', 'Search backward' },
+}, {
+  mode = {'n', 'x'},
+})
 
 -- window
-map('n', '\\', '<C-w>', {noremap=true})
+register({
+  ['\\'] = { '<C-w>', 'Window commands' },
+  ['\\w'] = {
+    function()
+      local current = api.nvim_get_current_win()
+      local windows = api.nvim_tabpage_list_wins(0)
+      local n = 0
 
-map('n', '\\w', function()
-  local current = api.nvim_get_current_win()
-  local windows = api.nvim_tabpage_list_wins(0)
-  local n = 0
+      for i, w in ipairs(windows) do
+        if w == current then
+          n = i
+          break
+        end
+      end
 
-  for i, w in ipairs(windows) do
-    if w == current then
-      n = i
-      break
-    end
-  end
-
-  for i = 1, #windows do
-    local w = windows[(n + i - 1) % #windows + 1]
-    if w == current then
-      break
-    end
-    if api.nvim_win_get_config(w).relative == '' then
-      api.nvim_set_current_win(w)
-      break
-    end
-  end
-end)
+      for i = 1, #windows do
+        local w = windows[(n + i - 1) % #windows + 1]
+        if w == current then
+          break
+        end
+        if api.nvim_win_get_config(w).relative == '' then
+          api.nvim_set_current_win(w)
+          break
+        end
+      end
+    end,
+    'Cycle windows',
+  }
+})
 
 -- folding
-map('n', '<Leader>=', 'za', {noremap=true})
+register({
+  ['<Leader>='] = { 'za', 'Toggle fold' },
+})
 
 -- navigation
-map('n', '<BS>', '<C-o>', {noremap=true})
+register({
+  ['<BS>'] = { '<C-o>', 'Go to previous jump position' },
+})
