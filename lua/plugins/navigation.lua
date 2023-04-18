b2b0af017c80449e26265872f4f3aadb12c2653e
cynix <cynix@cynix.org>
2023-04-18 11:21:30 +1000

    Add nvim-spider

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -10,6 +10,14 @@ return {
     end,
   },
   {
+    'chrisgrieser/nvim-spider',
+    keys = {
+      { 'w', '<Cmd>lua require("spider").motion("w")<CR>', desc='Spider-w', mode={'n', 'o', 'x'} },
+      { 'e', '<Cmd>lua require("spider").motion("e")<CR>', desc='Spider-e', mode={'n', 'o', 'x'} },
+      { 'b', '<Cmd>lua require("spider").motion("b")<CR>', desc='Spider-b', mode={'n', 'o', 'x'} },
+    },
+  },
+  {
     'ggandor/leap.nvim',
     -- do not override s
     keys = function()
