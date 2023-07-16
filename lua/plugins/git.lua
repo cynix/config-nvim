2fb9f72749f347d36d3cc3fcf8fea34cf102caad
cynix <cynix@cynix.org>
2023-07-17 09:50:34 +1000

    Use the same keymap to toggle diffview

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -19,8 +19,14 @@ return {
     'sindrets/diffview.nvim',
     dependencies = 'nvim-lua/plenary.nvim',
     keys = {
-      { '<Leader>gd', '<Cmd>DiffviewOpen<CR>', desc='DiffViewOpen' },
-      { '<Leader>gD', '<Cmd>DiffviewClose<CR>', desc='DiffViewClose' },
+      { '<Leader>gd', function()
+          local d = require('diffview')
+          if require('diffview.lib').get_current_view() then
+            d.close()
+          else
+            vim.cmd('DiffviewOpen')
+          end
+        end, desc='DiffView' },
       { ']x', desc='Next conflict' },
       { '[x', desc='Previous conflict' },
       { '<Leader>co', desc='Choose Ours' },
