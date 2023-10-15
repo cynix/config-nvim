9dadb84e812859b76b8d491127b2e77ae6439b5d
cynix <cynix@cynix.org>
2023-10-15 11:47:35 +1100

    Update dashboard-nvim repo

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -2,6 +2,6 @@ return {
   { 'catppuccin/nvim', enabled = false },
   { 'echasnovski/mini.surround', enabled = false },
   { 'folke/tokyonight.nvim', enabled = false },
-  { 'glepnir/dashboard-nvim', enabled = false },
+  { 'nvimdev/dashboard-nvim', enabled = false },
   { 'rafamadriz/friendly-snippets', enabled = false },
 }
