6d2caa9183055e74f91cdd6f33770bd39f6c2c06
cynix <cynix@cynix.org>
2023-11-14 22:40:02 +1100

    Disable mason

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -4,4 +4,6 @@ return {
   { 'folke/tokyonight.nvim', enabled = false },
   { 'nvimdev/dashboard-nvim', enabled = false },
   { 'rafamadriz/friendly-snippets', enabled = false },
+  { 'williamboman/mason.nvim', enabled = false },
+  { 'williamboman/mason-lspconfig.nvim', enabled = false },
 }
