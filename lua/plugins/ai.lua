12e81fb7fe4940854c7f86ab403a414a0a7bc10b
Brian Chu <brian.chu@optiver.com.au>
2025-06-03 13:24:06 +1000

    Add CodeCompanion

---
+++ lua/plugins/ai.lua
@@ -0,0 +1,53 @@
+return {
+  {
+    'echasnovski/mini.diff',
+    config = function()
+      local diff = require('mini.diff')
+      diff.setup({
+        source = diff.gen_source.none(),
+      })
+    end,
+  },
+  {
+    'OXY2DEV/markview.nvim',
+    ft = { 'markdown', 'codecompanion' },
+    opts = {
+      preview = {
+        filetypes = { 'markdown', 'codecompanion' },
+        ignore_buftypes = {},
+      },
+    },
+  },
+  {
+    'olimorris/codecompanion.nvim',
+    enabled = vim.g.ai_cmp,
+    keys = {
+      { '<Leader>a', '<cmd>CodeCompanionChat Toggle<CR>', desc='CodeCompanion Chat', mode={'n'} },
+      { '<Leader>a', '<cmd>CodeCompanionChat Add<CR>', desc='CodeCompanion Chat', mode={'v'} },
+      { '<Leader>i', ':CodeCompanion #buffer ', desc='CodeCompanion Inline', mode={'v'} },
+    },
+    opts = {
+      adapters = {
+        opts = {
+          proxy = vim.g.copilot_proxy,
+        },
+      },
+      strategies = {
+        inline = {
+          keymaps = {
+            accept_change = {
+              modes = { n = 'y' },
+            },
+            reject_change = {
+              modes = { n = 'n' },
+            },
+          },
+        },
+      },
+    },
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-treesitter/nvim-treesitter',
+    },
+  },
+}
